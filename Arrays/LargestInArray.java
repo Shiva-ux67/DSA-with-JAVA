e6ef55d379a503b8a7d73e0c44cc5ccae6e13cac
Shiva Prasad <jillapellyshivaprasad@gmail.com>
2025-08-18 20:58:58 +0530

    maximum element is in array

---
+++ Arrays/LargestInArray.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class LargestInArray {
+    public static void main(String[] args) {
+        int[] arr = {99,3,5,2,7,8,14,54};
+        int max = 0;
+        for(int i = 0;i < arr.length;i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println("largest element is the: "+max);
+    }
+}
