d9f7de629dbd9267d7f31ab378bed1790db8fa22
Shiva Prasad <jillapellyshivaprasad@gmail.com>
2025-09-21 05:33:52 +0530

    pallindrome programe

---
+++ Strings/pallindrome.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class pallindrome {
+    public static void main(String[] args) {
+        
+        String name = "racecbr";
+        int n = name.length();
+        for(int i = 0; i < n / 2;i++){
+            if(name.charAt(i) != name.charAt(n -1 - i)){
+                System.out.println("It is not pallindrome");
+                return;
+            }
+        }
+        System.out.println("It is pallindrome");
+    }
+}
