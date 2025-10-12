ffe0b431abef986fda3d52b7c76fdb3be97172fe
Shiva Prasad <jillapellyshivaprasad@gmail.com>
2025-10-12 15:58:10 +0530

    patterns

---
+++ patterns/First.java
@@ -0,0 +1,20 @@
+package pattens;
+
+public class First {
+    public static void main(String[] args) {
+        int n = 5;
+        int num = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || i == n - 1 || j == 0 || j == n -1  ){
+                    System.out.print("*");
+                    num++;
+                }else{
+
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
